f6c422c27a69de9a7877316d16b685cca89f2253
William Fernando Sampaio Santos <william.sampaio@actdigital.com>
2025-08-24 23:13:39 -0300

    feat: Create user

--- Projeto.DesenvolvimentoEstudo.Application/Users/Queries/ListUserQuery.cs
+++ Projeto.DesenvolvimentoEstudo.Application/Users/Queries/ListUserQuery.cs
@@ -1,14 +1,14 @@
 ﻿using MediatR;
-using Projeto.DesenvolvimentoEstudo.Application.Users.Requests;
+using Projeto.DesenvolvimentoEstudo.Application.Users.Commands;
 using Projeto.DesenvolvimentoEstudo.Domain.Repositories.Users;
 
 namespace Projeto.DesenvolvimentoEstudo.Application.Users.Queries;
 
 public class ListUserQuery : IRequest<IEnumerable<GetAllResponse>>
 {
-    public GetAllRequest _filter { get; set; }
+    public GetAllCommand _filter { get; set; }
 
-    public ListUserQuery(GetAllRequest filter)
+    public ListUserQuery(GetAllCommand filter)
     {
         _filter = filter;
     }
--- Projeto.DesenvolvimentoEstudo.Domain/Repositories/Users/IUserRepository.cs
+++ Projeto.DesenvolvimentoEstudo.Domain/Repositories/Users/IUserRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace Projeto.DesenvolvimentoEstudo.Domain.Repositories.Users;
+﻿using Projeto.DesenvolvimentoEstudo.Domain.Entities;
+
+namespace Projeto.DesenvolvimentoEstudo.Domain.Repositories.Users;
 
 public interface IUserRepository
 {
     Task<IEnumerable<GetAllResponse>> ListAsync(IGetAllRequest filter);
+
+    /// <summary>
+    ///     Creates a new user in the repository
+    /// </summary>
+    /// <param name="user">The user to create</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>The created user</returns>
+    Task<User> CreateAsync(User user, CancellationToken cancellationToken = default);
 }
