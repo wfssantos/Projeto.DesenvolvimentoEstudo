1bacbd50162ea74179b96e7062eb73aa1a50ee88
William Fernando Sampaio Santos <william.sampaio@actdigital.com>
2025-11-05 16:43:57 -0300

    feat: Add blazor for CRUD

---
+++ Projeto.DesenvolvimentoEstudo.WebBlazor/Enums/ProductStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Projeto.DesenvolvimentoEstudo.WebBlazor.Enums;
+
+/// <summary>
+/// Produts Status
+/// </summary>
+public enum ProductStatus
+{
+    Unknown = 0,
+    Active = 1,
+    Inactive = 2
+}
---
+++ Projeto.DesenvolvimentoEstudo.WebBlazor/Models/CompanyProduct.cs
@@ -0,0 +1,15 @@
+﻿using Projeto.DesenvolvimentoEstudo.WebBlazor.Enums;
+
+namespace Projeto.DesenvolvimentoEstudo.WebBlazor.Models;
+
+public class CompanyProduct
+{
+    public Guid CompanyId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; } = 0;
+    public int StockQuantity { get; set; } = 0;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime? UpdatedAt { get; set; } = null;
+    public ProductStatus Status { get; set; }
+}
